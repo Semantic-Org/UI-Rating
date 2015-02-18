6eefb3dece42789f75cfe9fc9b6cfa71b3e86056
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:26:00 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-rating',
+  summary : 'Semantic UI - Rating (official): Single component release of rating',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Rating.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
