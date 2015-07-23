3522dbfdb89727fbeb8e0c5554affa239f41470b
Pusher Robot <jack@semantic-ui.com>
2015-07-23 17:41:26 -0400

    Updated component to version 2.0.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-rating',
   summary : 'Semantic UI - Rating: Single component release',
-  version : '2.0.5',
+  version : '2.0.7',
   git     : 'git://github.com/Semantic-Org/UI-Rating.git',
 });
 
