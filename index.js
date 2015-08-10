d1b0379891ebc05a673e2bca5c242360178f690d
Pusher Robot <jack@semantic-ui.com>
2015-08-10 14:44:49 -0400

    Updated component to version 2.0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * # Semantic UI 2.0.7 - Rating
+ * # Semantic UI 2.0.8 - Rating
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -419,7 +419,7 @@ module.exports = function(parameters) {
   ;
 };
 
-module.exports.settings = {
+_module.exports.settings = {
 
   name          : 'Rating',
   namespace     : 'rating',
